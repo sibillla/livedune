0cb28fd889bf84c8dccc8b4010fc568ffc245994
iroda <iroda.turazhanova@stm-labs.ru>
2022-12-25 18:18:00 +0300

    Added prettier, install packages

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false
+}
