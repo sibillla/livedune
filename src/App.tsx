b53947dde6a55af42d97660b6cb10845794d4588
iroda <iroda.turazhanova@stm-labs.ru>
2022-12-25 18:58:16 +0300

    Added routing to app and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {MainTemplate} from 'shared/ui/templates/MainTemplate'
+import {NotFoundPage} from 'pages/NotFoundPage/NotFoundPage'
+import {RequireAuth} from 'shared/routes/RequireAuth'
+import {routes} from 'shared/routes'
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          {routes.map(({path, element, requireAuth}) => (
+            <Route
+              key={path}
+              path={path}
+              element={
+                <RequireAuth requireAuth={!!requireAuth}>
+                  <MainTemplate>{element}</MainTemplate>
+                </RequireAuth>
+              }
+            />
+          ))}
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
-export default App;
